4268141f170024f9135674f769a079fab442b29d
Kien Le <lekien.2004nek@gmail.com>
2023-02-13 11:38:46 +0700

    first commit

---
+++ music-list.js
@@ -0,0 +1,45 @@
+let allMusic = [
+    {
+      name: "Đám Cưới Nha",
+      artist: "Hồng Thanh,Mie",
+      img: "Dam-Cuoi-Nha",
+      src: "Dam-cuoi-nha"
+    },
+   
+    {
+      name: "See Tình",
+      artist: "Hoàng Thùy Linh",
+      img: "See-Tinh",
+      src: "songs_See-Tinh"
+    },
+    {
+      name: "Chỉ Muốn Bên Em Lúc Này",
+      artist: "Huy Vạc",
+      img: "ChiMuonBenEmLucNay",
+      src: "ChiMuonBenEmLucNay"
+    },
+    {
+      name: "Hồng Nhan",
+      artist: "Jack",
+      img: "Hong-Nhan-Jack",
+      src: "Hong-Nhan-Jack"
+    },
+    {
+      name: "Chỉ Muốn Bên Em Thật Gần",
+      artist: "YLinh,Xám",
+      img: "Chi-Muon-Ben-Em-That-Gan",
+      src: "Chi-Muon-Ben-Em-That-Gan"
+    },
+    {
+      name: "Em Là Nhất",
+      artist: "ssss",
+      img: "Em-La-Nhat",
+      src: "Em-La-Nhat"
+    },
+    {
+      name: "Vì anh là gió",
+      artist: "Lilk",
+      img: "ViAnhLaGio-LilK-4766315",
+      src: "ViAnhLaGio-LilK-4766315"
+    },
+  ];
