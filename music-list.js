a7b0895647878f77aa2f84d073907520c8e5a3b8
Kien Le <lekien.2004nek@gmail.com>
2023-02-13 20:50:09 +0700

    new

--- music-list.js
+++ music-list.js
@@ -1,45 +1,64 @@
-let allMusic = [
-  {
-    name: "=",
-    artist: "Hồng Thanh,Mie",
-    img: "Dam-Cuoi-Nha",
-    src: "Dam-cuoi-nha"
-  },
- 
-  {
-    name: "See Tình",
-    artist: "Hoàng Thùy Linh",
-    img: "see-tinh",
-    src: "songs_See-Tinh"
-  },
-  {
-    name: "Để Quên",
-    artist: "Ngọt",
-    img: "",
-    src: "dequen"
-  },
-  {
-    name: "nangdalammaemthemhong",
-    artist: ":))knho",
-    img: "bg",
-    src: "nangdalammaemthemhong"
-  },
-  {
-    name: "Như Anh Mơ",
-    artist: "PC",
-    img: "nhuanhmo",
-    src: "Như Anh Mơ"
-  },
-  {
-    name: "Tan ka ka ",
-    artist: "Đen cùng đồng bọn",
-    img: "ta",
-    src: "Tan ka aka"
-  },
-  {
-    name: "Từ ngày em đến",
-    artist: "Dalab",
-    img: "25",
-    src: "tungayemden"
-  },
+let
+allMusic = [
+    {
+        name: "=",
+        artist: "Hồng Thanh,Mie",
+        img: "Dam-Cuoi-Nha",
+        src: "Dam-cuoi-nha"
+    },
+
+    {
+        name: "See Tình",
+        artist: "Hoàng Thùy Linh",
+        img: "seetinh",
+        src: "seetinh"
+    },
+    {
+        name: "Để Quên",
+        artist: "Ngọt",
+        img: "dequen",
+        src: "dequen"
+    },
+    {
+        name: "nangdalammaemthemhong",
+        artist: ":))knho",
+        img: "nangdelammaemthemhong",
+        src: "nangdalammaemthemhong"
+    },
+    {
+        name: "Như Anh Mơ",
+        artist: "PC",
+        img: "nhuanhmo",
+        src: "nhuanhmo"
+    },
+    {
+        name: "Tan ka ka ",
+        artist: "Đen cùng đồng bọn",
+        img: "tankaaka",
+        src: "tankaaka"
+    },
+    {
+        name: "Từ ngày em đến",
+        artist: "Dalab",
+        img: "tungayemden",
+        src: "tungayemden"
+    },
+    {
+      name: "Dancing In My Room",
+      artist: "347aidan",
+      img: "dancinginmyromm",
+      src: "dancinginmyroom"
+    },
+    {
+      name: "Reeves",
+      artist: "MAMBO,HIEUTHUHAI,...",
+      img: "reeves",
+      src: "reeves"
+    },
+    {
+      name: "25",
+      artist: "Nhiều người",
+      img: "25",
+      src: "25"
+    },
 ];
